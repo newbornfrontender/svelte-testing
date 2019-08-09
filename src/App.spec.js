57a3b02e221837985a5dc9047789fdb423514a19
Infant Frontender <newbornfrontender@gmail.com>
2019-08-09 17:34:14 +0300

    feat(app): rewrite test

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import App from './App.svelte';
 import { render, wait } from '@testing-library/svelte';
 
-describe('render', () => {
+describe('app component', () => {
   it('programmatically change props', async () => {
     expect.hasAssertions();
 
@@ -16,17 +16,20 @@ describe('render', () => {
   });
 
   it('renders correctly', () => {
+    expect.hasAssertions();
+
     const { container } = render(App, { props: { name: 'world' } });
     const title = container.querySelector('h1').innerHTML;
 
     expect(title).toBe('Hello world!');
+
     expect(container).toMatchInlineSnapshot(`
       <body>
         <div>
           <h1
             class="svelte-12gyg4y"
           >
-            Hello 
+            Hello
             world
             !
           </h1>
